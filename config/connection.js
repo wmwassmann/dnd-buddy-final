e7fe4adeb69baf07855c083e4f4a7295d62c708a
William Wassmann <wmwassmann@hotmail.com>
2020-09-25 23:55:09 -0700

    fikggskidfngmlksdfg

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
