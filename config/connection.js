1c57e5e16699e4f0c0ed73f45246aa39a16c10f8
William Wassmann <wmwassmann@hotmail.com>
2020-09-26 01:41:03 -0700

    fgasdfasdf

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,10 @@
 var mysql = require("mysql");
 var connection;
 
-
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
-  connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,5 @@ CREATE TABLE cats
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	sleepy BOOLEAN DEFAULT false,
-	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
